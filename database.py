7fc7be26f13e7b5275aa817da5cf9c89b294bbd8
andrianm28 <andrianmaulana28@gmail.com>
2021-11-02 16:03:24 +0700

    db migration

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 
-DATABASE_URL = "postgresql://nvazmfctihttdo:fe1113f960fee9957c99b733975b77c9ad324a0723a20219153be7f83be55621@ec2-54-87-92-21.compute-1.amazonaws.com:5432/d37noquh5kj5dt"
+DATABASE_URL = "postgres://arlbteehpdfpcv:664de19fadbd55a9eda1cb3ea593c874003362a37ac6c9cc31ea3e3bd9867b50@ec2-34-202-178-115.compute-1.amazonaws.com:5432/d16ehj7c9cm6gi"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
