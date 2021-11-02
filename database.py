c0843177837610c08237b82f21a82abd59368727
andrianm28 <andrianmaulana28@gmail.com>
2021-11-02 16:08:56 +0700

    fix db url dialects

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 
-DATABASE_URL = "postgres://arlbteehpdfpcv:664de19fadbd55a9eda1cb3ea593c874003362a37ac6c9cc31ea3e3bd9867b50@ec2-34-202-178-115.compute-1.amazonaws.com:5432/d16ehj7c9cm6gi"
+DATABASE_URL = "postgresql://arlbteehpdfpcv:664de19fadbd55a9eda1cb3ea593c874003362a37ac6c9cc31ea3e3bd9867b50@ec2-34-202-178-115.compute-1.amazonaws.com:5432/d16ehj7c9cm6gi"
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
